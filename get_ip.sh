b0619c84ab7c623bfab82be175d91f471d6f6a76
Mayur Awaghade <mayurva@yahoo-inc.com>
2017-01-08 17:21:45 -0800

    Command and conditional check on the same line

--- get_ip.sh
+++ get_ip.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 for i in `seq 1 254`;
 do
-	ping 192.168.1.$i -q -c 1 >/dev/null 2>&1
-	if [ $? -eq 0 ]; then
+	if ping 192.168.1.$i -q -c 1 >/dev/null 2>&1; then
 		echo 192.168.1.$i
 	fi
 done
