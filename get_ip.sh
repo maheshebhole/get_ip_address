1eadb0fd3ea37d06477174e7deacce1f3cc801d3
Mayur Awaghade <mayurva@yahoo-inc.com>
2017-01-08 17:20:37 -0800

    Script should check exit code instead of STDOUT

--- get_ip.sh
+++ get_ip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for i in `seq 1 254`;
 do
-	rc=`ping 192.168.1.$i -c 1 | grep "ttl"`
-	if [[ ! -z $rc ]]; then
+	ping 192.168.1.$i -q -c 1 >/dev/null 2>&1
+	if [ $? -eq 0 ]; then
 		echo 192.168.1.$i
 	fi
 done
